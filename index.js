3e556910f5522a7d3b05e182c81dc9093656f01f
Mikko Rantanen <jubjub@jubjubnest.net>
2013-05-12 15:35:18 +0300

    Connecting with URL and reporting celt_versions

--- index.js
+++ index.js
@@ -2,106 +2,55 @@
 "use strict";
 
 var tls = require('tls');
+var mumbleutil = require('./lib/util');
 
-var MumbleConnection = require('./lib/MumbleConnection')
+var MumbleConnection = require('./lib/MumbleConnection');
 
 exports.MumbleConnection = MumbleConnection;
 
-var parseArgs = function( args, specs ) {
+exports.connect = function( url, options, done ) {
 
-    // Output arguments
-    var out = {};
-    var position = 1;
-    var current = 0;
-
-    for( var a in specs ) {
-        var spec = specs[a];
-        console.dir( out );
-        console.dir( spec );
-
-        // Read the required type.
-        var requiredType = spec.type || typeof spec.default;
-
-        // If there is no requirement on type use the first argument.
-        if( !requiredType ) {
-            out[ spec.name ] = args[ current ];
-            current++;
-        } else {
-
-            // We require a specific type. Make sure the argument matches.
-            var requiredTypes = requiredType.split('|');
-            var argType = typeof args[ current ];
-            var found = false;
-
-            for( var r in requiredTypes ) {
-                var candidate = requiredTypes[r];
-                console.log( 'type: ' + argType + ' vs candidate: ' + candidate );
-                if( argType === candidate ) {
-                    out[ spec.name ] = args[ current ];
-                    current++;
-                    found = true;
-                    break;
-                }
-            }
-
-            // We didn't find a candidate.
-            if( !found ) {
-                if( spec.default ) {
-                    out[ spec.name ] = spec.default;
-                } else {
-                    throw new TypeError( 'Required argument {' + spec.type + '} ' + spec.name + ' missing at position ' + position );
-                }
-            }
-        }
-
-        // We read an argument successfully.
-        position++;
+    if( typeof options === 'function' ) {
+        done = options;
+        options = {};
     }
 
-    return out;
-}
+    var server = mumbleutil.parseUrl( url );
 
-exports.connect = function( host, port, tlsoptions, done ) {
+    options = options || {};
 
-    var args = parseArgs( arguments, [
-        { name: 'host', default: 'localhost' },
-        { name: 'port', default: 64738, type: 'string|number' },
-        { name: 'tlsoptions', default: {} },
-        { name: 'done', type: 'function' }
-    ]);
-
-    host = args.host;
-    port = args.port;
-    tlsoptions = args.tlsoptions;
-    done = args.done;
-
-    console.log( host );
-    console.log( port );
-    console.log( tlsoptions );
-    console.log( done );
-
-    var args = [ host, port, tlsoptions, done ];
-    if( typeof args[0] !== 'string' ) {
-        host = 'localhost';
-    }
-
-    // If port wasn't given, use the default one.
-    if( !port ) { port = 64738; }
-
-    tlsoptions = tlsoptions || {};
-
-    // If the tlsoptions.rejectUnauthorized isn't defined default it to false.
+    // If the options.rejectUnauthorized isn't defined default it to false.
     // We'll do this since most Mumble server certs are self signed anyway.
     //
     // The if catches null, false and other falsy values as well,
     // but this doesn't affect anything as we set it to false anyway.
-    if( !tlsoptions.rejectUnauthorized ) {
-        tlsoptions.rejectUnauthorized = false;
+    if( !options.rejectUnauthorized ) {
+        options.rejectUnauthorized = false;
     }
 
-    var socket = tls.connect( port, host, tlsoptions, function () {
-        var connection = new MumbleConnection( socket );
+    var socket = tls.connect( server.port, server.host, options, function ( err ) {
+
+        // TODO Remove the certificate buffers from the options.
+        // MumbleConnection doesn't need to hold onto them.
+        var connection = new MumbleConnection( socket, options );
+
         done( null, connection );
+        if( !connection.authSent && server.username ) {
+            connection.authenticate( server.username );
+        }
+
+        // If path was given, wait for init to be done before moving.
+        if( server.path ) {
+            connection.once('initialized', function () {
+                setTimeout( function () {
+                    console.log( "Init done. Joining path\n\n");
+                    connection.joinPath( server.path );
+                }, 1000 );
+            });
+        }
     });
+
+    socket.on('error', done);
 }
 
+exports.celtVersions = mumbleutil.celtVersions;
